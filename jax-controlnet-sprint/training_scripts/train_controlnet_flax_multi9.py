a36d3c4ad75404ec02ab1deccaaaac619b28defb
lk-wq <55562097+lk-wq@users.noreply.github.com>
2023-05-30 17:02:51 -0500

    Update and rename train_controlnet_flax_multi8.py to train_controlnet_flax_multi9.py

--- jax-controlnet-sprint/training_scripts/train_controlnet_flax_multi9.py
+++ jax-controlnet-sprint/training_scripts/train_controlnet_flax_multi9.py
@@ -1286,7 +1286,7 @@ def main():
     text_param_spec = jax.tree_util.tree_map(lambda x: partition_shape(x.shape) , text_params)
     unet_param_spec = jax.tree_util.tree_map(lambda x: partition_shape(x.shape) , unet_params )
     control_param_spec = jax.tree_util.tree_map(lambda x: partition_shape(x.shape) , controlnet_params )
-    vae_spec = jax.tree_util.tree_map(lambda x: partition_shape(x.shape) , vae_params )
+    vae_param_spec = jax.tree_util.tree_map(lambda x: partition_shape(x.shape) , vae_params )
 
     text_params = jax.tree_util.tree_map(lambda x: jax.device_put(x ,NamedSharding(mesh , partition_shape(x.shape)) ).astype(jnp.bfloat16), text_params)
     unet_params = jax.tree_util.tree_map(lambda x: jax.device_put(x ,NamedSharding(mesh , partition_shape(x.shape)) ).astype(jnp.bfloat16), unet_params)
